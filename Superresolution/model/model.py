32b211ff97b78cf44cbec5fac8ab31c9ad1d90e0
lshaw8317 <61458341+lshaw8317@users.noreply.github.com>
2024-09-16 21:44:15 +0200

    Update model.py

--- Superresolution/model/model.py
+++ Superresolution/model/model.py
@@ -268,7 +268,7 @@ class DDPM(BaseModel):
                np.savez_compressed(fout,costmlmc=np.array(cost_mlmc),costmc=np.array(cost_mc))
 
             meanimg=torch.sum(sums[:,0]/dividerN[:,0,...],axis=0)#cut off one dummy axis
-            meanimg=Metrics.tensor2img(meanimg,min_max=(-1.,1)) #default min_max=(-1., 1.)
+            #meanimg=Metrics.tensor2img(meanimg,min_max=(-1.,1)) #default min_max=(-1., 1.), but causes problems for second moment
             # Write samples to disk or Google Cloud Storage
             with open(os.path.join(this_sample_dir, "meanpayoff.npz"), "wb") as fout:
                 np.savez_compressed(fout, meanpayoff=meanimg)
